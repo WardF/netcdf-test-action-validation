9b71b6f6a001910d858327d0657bf25942c35c81
Ward Fisher <wfisher@ucar.edu>
2025-03-20 14:27:51 -0600

    Add a new test for ctest-repeat, mnpich-version

--- run-validate-tests-locally.sh
+++ run-validate-tests-locally.sh
@@ -5,7 +5,7 @@ FVER=v4.6.1
 H5VER=1.14.6
 USECC=gcc
 ENVDIR="$(pwd)/environments"
-
+set -e 
 dosummary() {
     echo "Testing Macro Functionality"
     echo "==========================="
@@ -24,15 +24,15 @@ dosummary() {
 dosummary
 
 rm -rf netcdf-c netcdf-fortran
-git clone git@github.com:Unidata/netcdf-c && cd netcdf-c && git checkout ${CVER} $$ cd ..
-git clone git@github.com:Unidata/netcdf-fortran && cd netcdf-fortran && git checkout ${FVER} $$ cd ..
+git clone git@github.com:Unidata/netcdf-c && cd netcdf-c && git checkout ${CVER} && cd ..
+git clone git@github.com:Unidata/netcdf-fortran && cd netcdf-fortran && git checkout ${FVER} && cd ..
 
 ###
 # Test NetCDF-c
 ###
-docker run --rm -it -e GITHUB_ACTIONS=TRUE -e REPO_TYPE="c" -e FBRANCH=${FVER} -v $(pwd)/netcdf-c:/github/workspace -e H5VER=${H5VER} -e USE_CC=${USECC} -e USE_BUILDSYSTEM=both -v ${ENVDIR}:/environments  docker.unidata.ucar.edu/nctests
+docker run --rm -it -e GITHUB_ACTIONS=TRUE -e REPO_TYPE=c -e FBRANCH=${FVER} -v $(pwd)/netcdf-c:/github/workspace -e H5VER=${H5VER} -e USE_CC=${USECC} -e USE_BUILDSYSTEM=both -v ${ENVDIR}:/environments  docker.unidata.ucar.edu/nctests
 
 ###
 # Test NetCDF-fortran
 ###
-docker run --rm -it -e GITHUB_ACTIONS=TRUE -e REPO_TYPE="fortran" -e CBRANCH="${CVER} -v $(pwd)/netcdf-fortran:/github/workspace -e H5VER=${H5VER} -e USE_CC=${USECC} -e USE_BUILDSYSTEM=both -v ${ENVDIR}:/environments  docker.unidata.ucar.edu/nctests
+docker run --rm -it -e GITHUB_ACTIONS=TRUE -e REPO_TYPE=fortran -e CBRANCH=${CVER} -v $(pwd)/netcdf-fortran:/github/workspace -e H5VER=${H5VER} -e USE_CC=${USECC} -e USE_BUILDSYSTEM=both -v ${ENVDIR}:/environments  docker.unidata.ucar.edu/nctests
